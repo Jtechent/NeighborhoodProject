14bd1d1ae40f4f091c46c20b096c5014d4b737dc
Jeffrey Techentin <techentin4@gmail.com>
2016-10-10 10:42:32 -0400

    add: seed code for maps project

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var mapLogic = (function() {
+  //starter code
+  var map = new google.maps.Map(document.getElementById('map'),{
+    center: {lat:47.3769, lng:  8.5417},
+    zoom: 12
+  });
+})();
